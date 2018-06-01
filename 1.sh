24b2fcbd146c2ec78964205133c98982ad9e7a59
zhangchunli1 <39824698+zhangchunli1@users.noreply.github.com>
2018-06-01 03:52:30 -0500

    1.sh
    dou ni wan

---
+++ 1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+i=1
+a=0
+b=0
+while [  $i  -le   254 ]
+do
+  ping  -c2  -i0.1  -W1 172.40.55.$i &>/dev/null
+  if [  $?  -eq  0 ] ;  then
+  echo  "172.40.55.$i is  up"
+  let b++
+  else
+  echo "172.40.55.$i  is  down"
+  let a++
+  
+fi
+let i++
+done 
+echo "up $b"
+echo "down  $a"
